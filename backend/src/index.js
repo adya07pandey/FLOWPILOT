4e0d25b314cea4b3ec33092443e1506b2d57b5c3
Adya Pandey <adya07pandey@gmail.com>
2026-02-03 06:08:58 +0530

    Use VITE_API_URL for axis

--- backend/src/index.js
+++ backend/src/index.js
@@ -50,14 +50,11 @@ app.use("/", dashboardroutes);
 app.use(errorHandler);
 
 
-const PORT = 5000;
-
-
-
+const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
-    
-})
+    console.log(`Server running on port ${PORT}`);
+});
 
 
 // Handle unhandled promise rejections (e.g., database connection errors)
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BASE_PATH,
+  baseURL: "https://flowpilot-3if5.onrender.com",
   withCredentials: true,
 });
 
